14a38ac2d6580dbdad0738414f0e3e1fb45b3bca
K-Ramberg <kramberg0350@gmail.com>
2018-06-25 15:10:30 -0400

    ports running concurrently

--- helicon-app/app.js
+++ helicon-app/app.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+
+const connection = mongoose.connection
+connection.on('connected', ()=> {
+    console.log('Mongoose Connected')
+})
+connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ' + err)
+  })
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +23,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(__dirname + '/client/build'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
