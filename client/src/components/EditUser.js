7b67456d5311d9aed6d8f5d59d75b4f9b32ebe39
K-Ramberg <kramberg0350@gmail.com>
2018-06-26 10:42:12 -0400

    added user info update function

--- client/src/components/EditUser.js
+++ client/src/components/EditUser.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 class EditUser extends Component {
+    state = {
+        user: {
+            name: '',
+            description: ''
+        }
+    }
+
+    componentDidMount() {
+        const userId = this.props.match.params.userId
+        axios.get(`/api/users/${userId}`).then((res) => {
+            this.setState({
+                user: res.data.user
+            })
+        })
+    }
+
+    handleUserInfoChange(event) {
+        const keyNameValueOfInput = event.target.name
+        const userInput = event.target.value
+        const cloneState = {...this.state}
+        cloneState.user[keyNameValueOfInput] = userInput
+        this.setState(cloneState)
+    }
+
+    handleUserInfoUpdateSubmit() {
+
+    }
+
     render() {
+
+        const user = this.state.user
         return (
             <div>
-                this is teh edit
+                <div>
+                    <label htmlFor="name">Change name</label>
+                    <input type="text"
+                        name="name"
+                        value={user.name}
+                        onChange={(event) => this.handleUserInfoChange(event)} />
+                </div>
+                <div>
+                    <label htmlFor="description">Change About Me</label>
+                    <input type="text"
+                        name="description"
+                        value={user.description}
+                        onChange={(keyPressEvent) => this.handleUserInfoChange(keyPressEvent)} />
+                </div>
+                <Link to={`/users/${user._id}`}>Cancel</Link>
             </div>
         );
     }
